e6d0281e0ff91cd1d8ce9a8a913f25bcbf793850
PMagoga <pmagoga@gmail.com>
2023-09-30 20:16:19 -0300

    Adicionando a pasta SwitchCase

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("##Estrutura switch-case##");
+
+Console.WriteLine("Informe o nome do mês\t");
+
+var mes = Console.ReadLine().ToLower();
+
+switch (mes)
+{
+    case "janeiro":
+    case "março":
+    case "maio":
+    case "julho":
+    case "agosto":
+    case "outubro":
+    case "dezembro":
+        Console.WriteLine("Este mês tem 31 dias");
+        break;
+    case "fevereiro":
+        Console.WriteLine("Estes mês tem 28 dias");
+        break;
+    default:
+        Console.WriteLine("Este mês tem 30 dias");
+        break;
+}
+Console.WriteLine("\nFim do processamento");
+Console.ReadKey();
