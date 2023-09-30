67c8f7c3599392f527be6aba3bd44d8537ccc7d8
PMagoga <pmagoga@gmail.com>
2023-09-30 20:00:15 -0300

    Iniciando o repositório de estudos

---
+++ DeclaracaoVariaveis/Program.cs
@@ -0,0 +1,19 @@
+﻿//Crie uma variável chamada idade e atribua a ela o valor 35 e exiba o seu valor.
+
+int idade = 25;
+Console.WriteLine(idade);
+
+//Crie uma variável chamada nome e atribua o valor "Maria" e exiba o seu valor.
+
+string nome = "Maria";
+Console.WriteLine(nome);
+
+//Crie uma variável chamada altura e atribua a ela o valor 3.45.
+
+double altura = 3.45;
+Console.WriteLine(altura);
+
+//Crie uma variável chamada data e atribua a ela a data 04/09/1999 e exiba o seu valor.
+
+DateTime data = new DateTime(1999, 09, 04);
+Console.WriteLine(data);
