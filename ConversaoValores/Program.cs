68fa2e3f22d23a2c5d6d39ab28bcb0f6c782831e
PMagoga <pmagoga@gmail.com>
2023-10-04 20:53:26 -0300

    "Adicionando conversor graus"

--- ConversaoValores/Program.cs
+++ ConversaoValores/Program.cs
@@ -17,3 +17,22 @@ Console.WriteLine($"Divida value2 por value3, mnostra o resultado como um decima
 // O código do resultado3
 float result3 = value3 / value1;
 Console.WriteLine($"Divida value3 por value1, mostra o resultado como um float: {result3}");
+
+Console.WriteLine("------------------------------------------------");
+Console.WriteLine("------------------------------------------------");
+
+//Escreva um programa que solicite a temperatura em graus Celsius e converta para
+//Kelvin e Farhenheit usando as fórmulas a seguir:
+// - Converter para Kelvin => K = C + 273 ;
+//- Converter para Farhenheit => F = (C * 9) / 5 + 32 ;
+Console.WriteLine("Converte Grau Celsius para Farrenheit");
+Console.WriteLine("------------------------------------------------");
+
+Console.Write("Informe o valor em graus Celsius: ");
+double celsius = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Graus Kelvin = {celsius + 273}");
+
+double farhenheit = (celsius * 9) / 5 + 32;
+Console.WriteLine($"Graus Fahrenheit = {farhenheit}");
+Console.ReadKey();
